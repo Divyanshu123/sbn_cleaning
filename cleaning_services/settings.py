ee46e405d45edc284a43ac37da0fd9a933a866f2
divyanshu <prakashdivyanshu9@gmail.com>
2021-09-17 16:07:23 +0530

    modified requirements.txt

--- cleaning_services/settings.py
+++ cleaning_services/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'cleaning_services.urls'
@@ -128,6 +128,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+ 
